0d0670d3f8fe3007598f70b3c21cc9a27cbf5a7d
Kotoha Nakayama <nakochi@ms.nakochi.me>
2023-05-25 13:18:45 +0900

    Initial Commit

---
+++ Lino/LinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinoApp.swift
+//  Lino
+//
+//  Created by 中山 琴葉 on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
